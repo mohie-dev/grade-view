fd26fc7357e31db18e45e1bbfbbed48cc3026a03
boda0-mohie <bodemohie@gmail.com>
2026-06-15 22:25:19 +0300

    fix: api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import { LoginCredentials, StudentResult } from '@/types/student';
 
-// const API_URL = process.env.NEXT_PUBLIC_API_URL;
-// Local: http://localhost:3000
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = 'https://grade-view.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
